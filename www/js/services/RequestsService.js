2346f549cdb606d1d94e70ac4f043e4cd3e3ae3e
Juan Brusco <juanbrusco_23@hotmail.com>
2017-01-26 18:16:35 -0300

    Initial commit

---
+++ www/js/services/RequestsService.js
@@ -0,0 +1,36 @@
+(function(){
+
+  angular.module('starter')
+    .service('RequestsService', ['$http', '$q', '$ionicLoading',  RequestsService]);
+
+  function RequestsService($http, $q, $ionicLoading){
+
+    var base_url = 'http://8acf929e.ngrok.io';
+
+    function register(device_token){
+
+      var deferred = $q.defer();
+      $ionicLoading.show();
+
+      $http.post(base_url + '/register', {'device_token': device_token})
+        .success(function(response){
+
+          $ionicLoading.hide();
+          deferred.resolve(response);
+
+        })
+        .error(function(data){
+          deferred.reject();
+        });
+
+
+      return deferred.promise;
+
+    };
+
+
+    return {
+      register: register
+    };
+  }
+})();
